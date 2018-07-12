7ecc622b1870e6f33a9a8f46e8b581400499bebe
tayurus <tayurus@gmail.com>
2018-07-12 21:23:16 +0300

    add item's link

--- src/Components/Item/Item.js
+++ src/Components/Item/Item.js
@@ -7,22 +7,30 @@ export class Item extends React.Component {
 
     render() {
         return (
-            <div class="Item">
-                <div class="Item__title">{this.props.item.title}</div>
-                <h3 class="Item__name">{this.props.item.name}</h3>
-                <div class="Item__stuffing">{this.props.item.stuffing}</div>
-                <div class="Item__portionCount">
-                    <b>{this.props.item.portionCount}</b> порций
-                </div>
-                {this.props.item.gift.map(item => <div class="Item__gift">{item}</div>)}
-                <div class="Item__nyamushka" />
-                <div class="Item__weight">
-                    <div class="Item__weight-value">
-                        {this.props.item.weight}
+            <div className="Item-wrapper">
+                <div className="Item">
+                    <div className="Item__title">{this.props.item.title}</div>
+                    <h3 className="Item__name">{this.props.item.name}</h3>
+                    <div className="Item__stuffing">{this.props.item.stuffing}</div>
+                    <div className="Item__portionCount">
+                        <b>{this.props.item.portionCount}</b> порций
                     </div>
-                    <div class="Item__weight-kg">кг</div>
+                    {this.props.item.gift.map(item => <div className="Item__gift">{item}</div>)}
+                    <div className="Item__nyamushka" />
+                    <div className="Item__weight">
+                        <div className="Item__weight-value">
+                            {this.props.item.weight}
+                        </div>
+                        <div className="Item__weight-kg">кг</div>
+                    </div>
+                </div>
+                <div className="Item__buy">
+                     Чего сидишь? Порадуй котэ, 
+                     <a className="Item__buy-link" href="#"> купи</a>
                 </div>
             </div>
+
+
         );
     }
 }
