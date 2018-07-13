abdd62e812a3768724f61cce034c48d56ef98806
Yury Tatarintsev <yury@realrussialimited.local>
2018-07-13 15:30:58 +0300

    add behavior when item selected

--- src/App.js
+++ src/App.js
@@ -1,41 +1,71 @@
 import React, { Component } from "react";
 import "./App.css";
-import { Item } from './Components/Item/Item';
-import 'bootstrap/dist/css/bootstrap.css';
+import { Item } from "./Components/Item/Item";
+import "bootstrap/dist/css/bootstrap.css";
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
             items: [
                 {
+                    id: 0,
                     title: "Сказочное заморское яство",
-                    titleHoverSelected: "Котэ не одобряет?",
+                    titleSelectedHover: "Котэ не одобряет?",
                     name: "Нямушка",
                     stuffing: "с фуа-гра",
                     portionCount: 10,
                     gift: ["мышь в подарок"],
-                    weight: "0,5"
+                    weight: "0,5",
+                    status: "notSelected",
+                    selectedText: "Печень утки разварная с артишоками."
                 },
                 {
+                    id: 1,
                     title: "Сказочное заморское яство",
-                    titleHoverSelected: "Котэ не одобряет?",
+                    titleSelectedHover: "Котэ не одобряет?",
                     name: "Нямушка",
                     stuffing: "с рыбой",
                     portionCount: 40,
                     gift: ["2 мыши в подарок"],
-                    weight: "2"
+                    weight: "2",
+                    status: "notSelected",
+                    selectedText: "Головы щучьи с чесноком да свежайшая сёмгушка."
                 },
                 {
+                    id: 2,
                     title: "Сказочное заморское яство",
-                    titleHoverSelected: "Котэ не одобряет?",
+                    titleSelectedHover: "Котэ не одобряет?",
                     name: "Нямушка",
                     stuffing: "с курой",
                     portionCount: 100,
                     gift: ["5 мышей в подарок", "заказчик доволен"],
-                    weight: "5"
+                    weight: "5",
+                    status: "disabled",
+                    selectedText: "Филе из цыплят с трюфелями в бульоне."
                 }
             ]
         };
+
+        this.toggleStatus = this.toggleStatus.bind(this);
+        this.getItemIndexById = this.getItemIndexById.bind(this);
+    }
+
+    toggleStatus(id) {
+        let index = this.getItemIndexById(id);
+        let newItems = this.state.items;
+        if (newItems[index].status !== "disabled"){
+            newItems[index].status = (newItems[index].status === "notSelected") ? "selected" : "notSelected";
+            this.setState({ items: newItems });
+        }
+
+    }
+
+    getItemIndexById(id) {
+        let res = -1;
+        this.state.items.forEach((item, index) => {
+            if (item.id === id) res = index;
+        });
+        return res;
     }
 
     render() {
@@ -45,17 +75,16 @@ class App extends Component {
                 <div class="container px-2 px-xl-0">
                     <div class="row mx-0">
                         <div class="col-xl-4 d-flex justify-content-center mb-4 mb-xl-0">
-                            <Item item={this.state.items[0]}/>
+                            <Item item={this.state.items[0]} toggleStatus={this.toggleStatus} />
                         </div>
                         <div class="col-xl-4 col-12 col-md-6 d-flex justify-content-center mb-4 mb-md-0">
-                            <Item item={this.state.items[1]}/>
+                            <Item item={this.state.items[1]} toggleStatus={this.toggleStatus} />
                         </div>
                         <div class="col-xl-4 col-12 col-md-6 d-flex justify-content-center mb-4 mb-md-0">
-                            <Item item={this.state.items[2]}/>
+                            <Item item={this.state.items[2]} toggleStatus={this.toggleStatus} />
                         </div>
                     </div>
                 </div>
-
             </div>
         );
     }
--- src/Components/Item/Item.js
+++ src/Components/Item/Item.js
@@ -5,11 +5,29 @@ export class Item extends React.Component {
         super(props);
     }
 
+    showSelectedHoverTitle(text, textColor) {
+        if (this.refs.mainWrapper.classList.contains("selected")){
+            this.refs.title.innerHTML = text;
+            this.refs.title.style.color = "#e62e7a";
+        }
+        else {
+            this.refs.title.innerHTML = this.props.item.title;
+        }
+    }
+
     render() {
         return (
-            <div className="Item-wrapper">
-                <div className="Item">
-                    <div className="Item__title">{this.props.item.title}</div>
+            <div className={"Item-wrapper " + this.props.item.status} ref="mainWrapper">
+                <div
+                    className="Item"
+                    onClick={() => this.props.toggleStatus(this.props.item.id) }
+                    onMouseEnter={() => this.showSelectedHoverTitle(this.props.item.titleSelectedHover)}
+                    onMouseLeave={() => this.showSelectedHoverTitle(this.props.item.title)}
+                >
+                    <div class="Item__inner-wrapper" />
+                    <div className="Item__title" ref="title">
+                        {this.props.item.title}
+                    </div>
                     <h3 className="Item__name">{this.props.item.name}</h3>
                     <div className="Item__stuffing">{this.props.item.stuffing}</div>
                     <div className="Item__portionCount">
@@ -18,19 +36,19 @@ export class Item extends React.Component {
                     {this.props.item.gift.map(item => <div className="Item__gift">{item}</div>)}
                     <div className="Item__nyamushka" />
                     <div className="Item__weight">
-                        <div className="Item__weight-value">
-                            {this.props.item.weight}
-                        </div>
+                        <div className="Item__weight-value">{this.props.item.weight}</div>
                         <div className="Item__weight-kg">кг</div>
                     </div>
                 </div>
-                <div className="Item__buy">
-                     Чего сидишь? Порадуй котэ, 
-                     <a className="Item__buy-link" href="#"> купи</a>
+                <div className="Item__label Item__label_buy">
+                    Чего сидишь? Порадуй котэ,
+                    <a className="Item__label-link" href="#" onClick={() => this.props.toggleStatus(this.props.item.id)}>
+                        купи
+                    </a>
                 </div>
+                <div className="Item__label Item__label_sadness">{"Печалька, " + this.props.item.stuffing + " закончился."}</div>
+                <div className="Item__label Item__label_selected">{this.props.item.selectedText}</div>
             </div>
-
-
         );
     }
 }
